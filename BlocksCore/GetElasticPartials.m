c5a96c82a297e5e05b57452163543c07854d7497
jploveless <jloveles@smith.edu>
2014-08-27 15:18:10 -0400

    GetElasticPartials; variable saving changes for global models
    Changed GetElasticPartials to do the segment-local oblique Mercator
    within the for loop as opposed to the previous method of doing all
    segments and stations as part of a single matrix. This substantially
    reduces RAM usage for huge segment files.
    
    Added a few "-v7.3" flags to saving commands so that multiple
    gigabyte variables can be saved (elastic partials, for example).

--- BlocksCore/GetElasticPartials.m
+++ BlocksCore/GetElasticPartials.m
@@ -16,28 +16,31 @@ nSta                                        = numel(S.lon);
 nSeg                                        = numel(F.lon1);
 
 % Allocate space for partials
+Strike                                    = zeros(nSeg, 1);
 G                                           = zeros(3*nSta, 3*nSeg);
 [v1 v2 v3]                                  = deal(cell(1, nSeg));
-
-% Calculate projected Cartesian coordinates
-[F, S]                                      = ProjectSegCoords(F, S);
-
-% Calculate fault parameters in Okada form
-[strike, L, W, ofx, ofy, ofxe, ofye, ...
-               tfx, tfy, tfxe, tfye]        = fault_params_to_okada_form(F.px1, F.py1, F.px2, F.py2, deg_to_rad(F.dip), F.lDep, F.bDep);
 
 
 % Loop through each segment and calculate displacement
 parfor (i = 1:nSeg)
+    f = structsubset(F, i);
+    % Calculate projected Cartesian coordinates
+    [f, s]                                  = ProjectSegCoords(f, S);
+
+    Strike(i)                               = f.strike;
+
+    % Calculate fault parameters in Okada form
+    [strike, L, W, ofx, ofy, ofxe, ofye, ...
+               tfx, tfy, tfxe, tfye]        = fault_params_to_okada_form(f.px1, f.py1, f.px2, f.py2, deg_to_rad(f.dip), f.lDep, f.bDep);
 
     % Displacements due to unit slip components
     [ves vns vus...
      ved vnd vud...
-     vet vnt vut]                           = okada_partials(ofx(i), ofy(i), strike(i), F.lDep(i), deg_to_rad(F.dip(i)), L(i), W(i), 1, 1, 1, S.fpx(:, i), S.fpy(:, i), nu);
+     vet vnt vut]                           = okada_partials(ofx, ofy, strike, f.lDep, deg_to_rad(f.dip), L, W, 1, 1, 1, s.fpx, s.fpy, nu);
      
     v1{i}                                   = reshape([ves vns vus]', 3*nSta, 1);
-    v2{i}                                   = reshape(sign(90 - F.dip(i)).*[ved vnd vud]', 3*nSta, 1);
-    v3{i}                                   = reshape((F.dip(i) - 90 == 0).*[vet vnt vut]', 3*nSta, 1); 
+    v2{i}                                   = reshape(sign(90 - f.dip).*[ved vnd vud]', 3*nSta, 1);
+    v3{i}                                   = reshape((f.dip - 90 == 0).*[vet vnt vut]', 3*nSta, 1); 
 
 end
 
@@ -48,4 +51,4 @@ G(:, 3:3:end)                               = cell2mat(v3);
 
 % Rotate displacement vectors to project back into global coordinate system
 % (removing effects of local oblique Mercator projection)
-G                                           = xyz2enumat(G, -F.strike + 90);
+G                                           = xyz2enumat(G, -Strike + 90);
--- BlocksCore/SaveKernels.m
+++ BlocksCore/SaveKernels.m
@@ -4,9 +4,9 @@ function SaveKernels(Partials, Command, varargin)
 if strcmp(Command.saveKernels, 'yes') == 1;
    if exist('tempkernels.mat', 'file')
       for i = 1:length(varargin)
-         save('tempkernels.mat', '-struct', 'Partials', varargin{i}, '-append');
+         save('tempkernels.mat', '-struct', 'Partials', varargin{i}, '-append', '-v7.3');
       end
    else
-      save('tempkernels.mat', '-struct', 'Partials');
+      save('tempkernels.mat', '-struct', 'Partials', '-v7.3');
    end
 end
