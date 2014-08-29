6cfd8eb9e909c36d590a5c5d6c8b2d039371c9b5
jploveless <jloveles@smith.edu>
2014-08-29 12:11:13 -0400

    Sped up/reduced memory usage for GetElasticPartials.m
    I moved the unprojection of calculated partials inside the for loop
    as opposed to the previous version in which the unprojection was done
    for all segments at once. On my machine, this allows partials to be
    calculated for the global model without using all 32 GB of RAM, and
    as a result, runtime is halved (from 200 seconds to 100 seconds).

--- BlocksCore/GetElasticPartials.m
+++ BlocksCore/GetElasticPartials.m
@@ -16,9 +16,12 @@ nSta                                        = numel(S.lon);
 nSeg                                        = numel(F.lon1);
 
 % Allocate space for partials
-Strike                                    = zeros(nSeg, 1);
+Strike                                      = zeros(nSeg, 1);
 G                                           = zeros(3*nSta, 3*nSeg);
 [v1 v2 v3]                                  = deal(cell(1, nSeg));
+col1                                        = 1:3:3*nSeg;
+col2                                        = 2:3:3*nSeg;
+col3                                        = 3:3:3*nSeg;
 
 
 % Loop through each segment and calculate displacement
@@ -41,14 +44,17 @@ parfor (i = 1:nSeg)
     v1{i}                                   = reshape([ves vns vus]', 3*nSta, 1);
     v2{i}                                   = reshape(sign(90 - f.dip).*[ved vnd vud]', 3*nSta, 1);
     v3{i}                                   = reshape((f.dip - 90 == 0).*[vet vnt vut]', 3*nSta, 1); 
-
+    
+     v1{i}                   = xyz2enumat((v1{i}), -f.strike + 90);
+     v2{i}                   = xyz2enumat((v2{i}), -f.strike + 90);
+     v3{i}                   = xyz2enumat((v3{i}), -f.strike + 90);
 end
-
-% Place cell arrays into the partials matrix
+% 
+% % Place cell arrays into the partials matrix
 G(:, 1:3:end)                               = cell2mat(v1);
 G(:, 2:3:end)                               = cell2mat(v2);
 G(:, 3:3:end)                               = cell2mat(v3);
-
-% Rotate displacement vectors to project back into global coordinate system
-% (removing effects of local oblique Mercator projection)
-G                                           = xyz2enumat(G, -Strike + 90);
+% 
+% % Rotate displacement vectors to project back into global coordinate system
+% % (removing effects of local oblique Mercator projection)
+% G                                           = xyz2enumat(G, -Strike + 90);
