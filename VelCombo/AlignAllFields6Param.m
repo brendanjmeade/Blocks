5e5ca1f6772bd7b267eddf8cb225dcb4d708ef70
jploveless <jloveles@smith.edu>
2014-08-29 12:08:53 -0400

    Sped up/reduced memory usage in GetElasticPartials.m
    I now moved the UNprojection of calculated partials inside the for
    loop as opposed to doing it for all segments at once. This finally
    results in no paging out for the global test model, with a speedup
    of about 2x (the previous version took 211 seconds on my machine and
    this new version takes 100 seconds).

--- VelCombo/AlignAllFields6Param.m
+++ VelCombo/AlignAllFields6Param.m
@@ -65,7 +65,7 @@ mkdir([direc runName])
 
 
 % find all .sta or .sta.data files in specified directory
-dirdata                             = dir([direc '*.sta']);
+dirdata                             = dir([direc '*.sta.data']);
 nfiles                              = numel(dirdata);
 nOverlap                            = zeros(nfiles);
 nOverlapi                           = zeros(nfiles);
@@ -210,7 +210,7 @@ end
 toRotate                            = setdiff(1:nfiles,target);
 
 % carry out all rotations
-keyboard
+
 for rots = 1:nfiles-1; 
    i                                = toRotate(rots); % define the index
    omega                            = zeros(5, 1);
@@ -311,3 +311,5 @@ WriteStation(filename, s.lon, s.lat, s.eastVel, s.northVel, s.eastSig, s.northSi
 % Save a few variables to a .mat file
 matname                       = sprintf('%s.mat', filename(1:end-9));
 save(matname, 'names', 'sumnStations', 'paths', '-mat');
+keyboard
+save('%s_allvariables.mat', filename(1:end-9));
