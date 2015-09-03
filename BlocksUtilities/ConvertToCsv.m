61fb101eafe1886e2f94f86beb2d3c44bd9f3f38
brendanjmeade <brendanjmeade@gmail.com>
2015-09-02 20:45:14 -0400

    ConvertToCsv now properly converts .block files too

--- BlocksUtilities/ConvertToCsv.m
+++ BlocksUtilities/ConvertToCsv.m
@@ -7,17 +7,19 @@ end
 
 
 function ConvertAll(file_type)
-% Look in current director for all files of current file_type
+% Look in current directory for all files of current file_type
 dir_data = dir(file_type);
 for i = 1:numel(dir_data)
     if strcmp(file_type, '*.sta')
         S = ReadStation(dir_data(i).name);
-    elseif strcmp(file_type, '*.segment');
+    elseif strcmp(file_type, '*.segment')
         S = ReadSegmentStruct(dir_data(i).name);
+    elseif strcmp(file_type, '*.block')
+        S = ReadBlock(dir_data(i).name);
     end
+
+    % Convert to csv
     fprintf(1, 'Converting %s to %s\n', ...
             dir_data(i).name, [dir_data(i).name, '.csv']);
     struct2csv(S, [dir_data(i).name, '.csv']);
 end
-
-
