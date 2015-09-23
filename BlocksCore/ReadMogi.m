31d8db815b5f020c30cd04163e4aec51a16b8dec
Jack Loveless <jloveles@smith.edu>
2015-09-23 15:47:36 -0400

    Changed Mogi I/O to comma-separated

--- BlocksCore/ReadMogi.m
+++ BlocksCore/ReadMogi.m
@@ -1,17 +1,17 @@
 function Mogi = ReadMogi(mogifile)
 % READMOGI  Reads in coordinates of Mogi sources
-%    M = READMOGI(file) reads a 5-column array from the 
+%    M = READMOGI(file) reads a 6-column array from the 
 %    specified file, of the format:
 %
-%    longitude | latitude | depth (km) | \DeltaV (km^3) | \DeltaVSigma (km^3)
+%    longitude, latitude, depth (km), toggle, \DeltaV (m^3), \DeltaVSigma (m^3)
 %
 %    The file is assumed to have a one-line header, and 
-%    the columns are assumed to be space-separated.
+%    the columns are assumed to be comma-separated.
 %
 
 if ~isempty(mogifile)
    fid        = fopen(mogifile, 'r');
-   m          = textscan(fid, '%f %f %f %f %f %f\n', 'headerlines', 1);
+   m          = textscan(fid, '%f, %f, %f, %f, %f, %f\n', 'headerlines', 1);
    Mogi.lon   = m{1};
    Mogi.lat   = m{2};
    Mogi.dep   = m{3};
--- BlocksCore/WriteMogi.m
+++ BlocksCore/WriteMogi.m
@@ -11,6 +11,6 @@ function WriteMogi(mogifile, Mogi, Model)
 
 
 fid        = fopen(mogifile, 'w');
-fprintf(fid, 'Longitude Latitude Depth DV_Flag DV DVS\r');
-fprintf(fid, '%g %g %g %g %g %g\r', [Mogi.lon, Mogi.lat, Mogi.dep, ones(size(Mogi.lon)), Model.mogiDeltaV, Model.mogiDeltaVSig]');
+fprintf(fid, 'Longitude, Latitude, Depth, DV_Flag, DV, DVS\r');
+fprintf(fid, '%g, %g, %g, %g, %g, %g\r', [Mogi.lon, Mogi.lat, Mogi.dep, ones(size(Mogi.lon)), Model.mogiDeltaV, Model.mogiDeltaVSig]');
 fclose(fid);
