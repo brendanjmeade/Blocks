5bfd38ed4f20d3a93015b19954ecedd5d9d12f51
Jack Loveless <jloveles@smith.edu>
2016-07-28 20:20:38 -0400

    Fixed WriteInputCopies.m to handle filenames with special characters

--- BlocksCore/WriteInputCopies.m
+++ BlocksCore/WriteInputCopies.m
@@ -8,9 +8,9 @@ function WriteInputCopies(c, direc)
 system(sprintf('mkdir %s', direc)); 
 
 % Copy the segment, block, and station file
-system(sprintf('cp %s %s%s.', c.segFileName, direc, filesep));
-system(sprintf('cp %s %s%s.', c.blockFileName, direc, filesep));
-system(sprintf('cp %s %s%s.', c.staFileName, direc, filesep));
+system(sprintf('cp ''%s'' %s%s.', c.segFileName, direc, filesep));
+system(sprintf('cp ''%s'' %s%s.', c.blockFileName, direc, filesep));
+system(sprintf('cp ''%s'' %s%s.', c.staFileName, direc, filesep));
 
 % Parse number of patch files
 spaces = strfind(strtrim(c.patchFileNames), ' ');
@@ -20,6 +20,6 @@ begs = [1 ends(1:end-1)+2];
 % Write patch files
 if ends ~= 0
    for i = 1:length(begs)
-      system(sprintf('cp %s %s%s.', c.patchFileNames(begs(i):ends(i)), direc, filesep));
+      system(sprintf('cp ''%s'' %s%s.', c.patchFileNames(begs(i):ends(i)), direc, filesep));
    end
 end
